78e4620794c9f4dc08a173217054e41aeff0c1ff
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2020-12-02 19:38:52 +0300

    this is an interesting way of doing things with the :after pseudo element. I'm not going to use this one, but saving it in any case

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
     const container = document.querySelector("#container");
 //Create 16x16=256 divs inside the container div
-    for (i=0; i< 256; i++) {
+    for (i=0; i<276; i++) {
         const gridArea= document.createElement("div");
         gridArea.className="squares";
        //gridArea.innerText="divs";
