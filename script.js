18d1ef5fdc7d4530f0c8a1ff46114536f74f1a3c
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2020-12-02 16:14:03 +0300

    added 16x16=256 divs for now, but whenever I delete the innertext, the divs collapse

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){
 
     const container = document.querySelector("#container");
-//Create 16 divs inside the container div
-    for (i=0; i<16; i++) {
+//Create 16x16=256 divs inside the container div
+    for (i=0; i<256; i++) {
         const gridArea= document.createElement("div");
         gridArea.className="gridDivs";
-        gridArea.innerText="divs";
+       gridArea.innerText="divs";
         container.appendChild(gridArea);
     };
   
