99b38937ef500e95bceef3cc941c7d7393a9540c
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2020-12-02 17:56:17 +0300

    added the lasting hover effect

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ document.addEventListener("DOMContentLoaded", function(){
 
     const container = document.querySelector("#container");
 //Create 16x16=256 divs inside the container div
-    for (i=0; i<256; i++) {
+    for (i=0; i< 256; i++) {
         const gridArea= document.createElement("div");
-        gridArea.className="gridDivs";
-      // gridArea.innerText="divs";
+        gridArea.className="squares";
+       //gridArea.innerText="divs";
         container.appendChild(gridArea);
+        gridArea.addEventListener("mouseover", 
+        e=> e.target.classList.add("hoverClass")
+        )
     };
-  
 
 
 
