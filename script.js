50afd5016f1d5c749bfd2aaa1cb2873e5dc5eac3
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2020-12-02 12:46:44 +0300

    created 16 divs with for loop

---
+++ script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    const container = document.querySelector("#container");
+//Create 16 divs inside the container div
+    for (i=0; i<16; i++) {
+        const gridArea= document.createElement("div");
+        gridArea.className="gridDivs";
+        container.appendChild(gridArea);
+    };
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  });
