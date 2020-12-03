1274a607593be34689f50c6db2827ef3f4d40daf
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2020-12-03 19:06:43 +0300

    made the code cleaner

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
 
     const container = document.querySelector("#container");
-    document.getElementById("container").style.width = "50vw";
-    document.getElementById("container").style.height = "50vw";
-    /*
-//Create 16x16=256 divs inside the container div
-    for (i=0; i<256; i++) {
-        const gridArea= document.createElement("div");
-        gridArea.className="squares";
-       //gridArea.innerText="divs";
-        container.appendChild(gridArea);
-        gridArea.addEventListener("mouseover", 
-        e=> e.target.classList.add("hoverClass")
-        )
-    };
-*/
+  
 function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
@@ -29,10 +16,13 @@ function makeRows(rows, cols) {
         
     };
   };
-  //document.getElementById("container").style.width = "300px";
+
+  document.getElementById("container").style.width = "50vw";
+  document.getElementById("container").style.height = "30vw";
+
   
 
-  makeRows(100, 100);
+  makeRows(16, 16);
 
 
 
