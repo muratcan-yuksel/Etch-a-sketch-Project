8731f82d8ce8d3215fbd59bd2d36837241996bb6
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2020-12-03 22:19:10 +0300

    cleaned the code a bit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
 
     const container = document.querySelector("#container");
-  
+  //core function to create divs 
 function makeRows(rows) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', rows);
@@ -19,27 +19,21 @@ function makeRows(rows) {
   document.getElementById("container").style.width = "50vw";
   document.getElementById("container").style.height = "30vw";
 
-  
+  //call the function initially
 makeRows(16);
 
-
-  
-
-
 //slider
-
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 output.innerHTML = slider.value + " x" + " " + slider.value; // Display the default slider value
-
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
   output.innerHTML = this.value;
-  //makeRows(this.value);
+  
 }
-
 //slider button
 let slideButton= document.getElementById("sliderButton");
+//updates the container/grids when the button is pressed
 slideButton.addEventListener("click", function(e){
     document.getElementById("container").innerHTML="";
     makeRows(output.innerHTML);
