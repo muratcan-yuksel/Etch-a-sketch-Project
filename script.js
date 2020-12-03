37e11c2b925501b893d8f7179afbc32f5dd1e94c
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2020-12-03 18:36:16 +0300

    I got help from the following link https://stackoverflow.com/questions/57550082/creating-a-16x16-grid-using-javascript and now the JS function is much more suitable for my purpose. I'll have to figure out how to keep the container width in check visa and only via JS now

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(){
 
     const container = document.querySelector("#container");
+    /*
 //Create 16x16=256 divs inside the container div
-    for (i=0; i<276; i++) {
+    for (i=0; i<256; i++) {
         const gridArea= document.createElement("div");
         gridArea.className="squares";
        //gridArea.innerText="divs";
@@ -11,7 +12,24 @@ document.addEventListener("DOMContentLoaded", function(){
         e=> e.target.classList.add("hoverClass")
         )
     };
+*/
+function makeRows(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+        const gridArea= document.createElement("div");
+        gridArea.className="squares";
+       //gridArea.innerText="divs";
+        container.appendChild(gridArea);
+        gridArea.addEventListener("mouseover", 
+        e=> e.target.classList.add("hoverClass")
+        )
+    };
+  };
+  //document.getElementById("container").style.width = "300px";
+  
 
+  makeRows(16, 16);
 
 
 
