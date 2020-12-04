808962c3ed49f4f3058c500698fe57b4439f1793
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2020-12-04 17:58:11 +0300

    added another button black, now the user has to choose which color s/he wants at start

--- script.js
+++ script.js
@@ -9,24 +9,24 @@ function makeRows(rows) {
         const gridArea= document.createElement("div");
         gridArea.className="squares";
         container.appendChild(gridArea);
-        gridArea.addEventListener("mouseover", 
-        e=> e.target.classList.add("hoverClass")
-        )
-        //blackish colorful hovering effect function 
+        //black color hovering
+        let blackButton= document.getElementById("black");
+        blackButton.addEventListener("click", function(e){
+            gridArea.addEventListener("mouseover", 
+            e=> e.target.classList.add("hoverClass")
+            )
+        })
+        //blackish techno color function
         let colorful = document.getElementById("rainbow");
 rainbow.addEventListener("click", function (e){
     gridArea.addEventListener("mouseover", function(e){
-
-    
     var color = '#'+Math.floor(Math.random()*16777215).toString(16);
-    var colorString = '0px 0px 30px 0px ' + color;
+    var colorString = '5px 5px 20px 5px ' + color;
     this.style['box-shadow'] = colorString;
     this.style['-webkit-box-shadow'] = colorString;
     this.style['-moz-box-shadow'] = colorString;
 })
-});
-
-        
+});     
     };
   };
 
@@ -43,9 +43,7 @@ output.innerHTML = slider.value + " x" + " " + slider.value; // Display the defa
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
   output.innerHTML = this.value;
-  
 }
-
 //slider button
 let slideButton= document.getElementById("sliderButton");
 //updates the container/grids when the button is pressed
